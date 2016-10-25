6b4ea179c587b27c83fe87082dca48f708c8bb75
Oscar Mañas <oscmansan@gmail.com>
2016-10-25 10:23:12 +0200

    Progress on layer unit-test

--- src/caffe/test/test_layer/test_layer.cpp
+++ src/caffe/test/test_layer/test_layer.cpp
@@ -2,6 +2,8 @@
 #include <string>
 using namespace std;
 
+#include "gtest/gtest.h"
+
 #include "caffe/blob.hpp"
 #include "caffe/vision_layers.hpp"
 #include "caffe/proto/caffe.pb.h"
@@ -9,38 +11,42 @@ using namespace std;
 #include "caffe/util/get.hpp"
 using namespace caffe;
 
-
-int main() {
-    string param_file = "conv_layer.prototxt";
-    LayerParameter param;
-    if (!ReadProtoFromTextFile(param_file, &param))
-        cout << "Failed to parse LayerParameter file: " << param_file << endl;
-    
-    ConvolutionLayer<float16,float16> layer(param);
-    ConvolutionParameter conv_param = layer.layer_param().convolution_param();
-
-    vector<Blob<float16,float16>*> bottom;
-    Blob<float16,float16>* bottom_blob = new Blob<float16,float16>(1,3,600,1000);
-    bottom.push_back(bottom_blob);
-    cout << "num_axes: " << bottom_blob->num_axes() << endl;
-    const int channel_axis_ = bottom_blob->CanonicalAxisIndex(conv_param.axis());
-    const int first_spatial_axis = channel_axis_ + 1;
-    const int num_axes = bottom_blob->num_axes();
-    const int num_spatial_axes_ = num_axes - first_spatial_axis;
-    cout << "firs_spatial_axis: " << first_spatial_axis << endl;
-    cout << "num_spatial_axes: " << num_spatial_axes_ << endl;
-
-    vector<Blob<float16,float16>*> top;
-    Blob<float16,float16>* top_blob = new Blob<float16,float16>(1,64,600,1000);
-    top.push_back(top_blob);
-
-    layer.LayerSetUp(bottom, top);
-    layer.Forward(bottom, top);
-    
-    vector<int> shape = top[0]->shape();
+void print_shape(Blob<float16,float16>* blob) {
+    vector<int> shape = blob->shape();
     cout << "(" << shape[0];
     for (int i = 1; i < shape.size(); ++i) {
         cout << "," << shape[i];
     }
     cout << ")" << endl;
+}
+
+int main() {
+    vector<Blob<float16,float16>*> bottom;
+    Blob<float16,float16>* bottom_blob = new Blob<float16,float16>(2, 3, 7, 5);
+    bottom.push_back(bottom_blob);
+
+    vector<Blob<float16,float16>*> top;
+    Blob<float16,float16>* top_blob = new Blob<float16,float16>();
+    top.push_back(top_blob);
+
+    LayerParameter layer_param;
+    ConvolutionParameter* conv_param = layer_param.mutable_convolution_param();
+    conv_param->add_kernel_size(3);
+    conv_param->add_stride(2);
+    conv_param->set_num_output(4); // number of filters
+    
+    shared_ptr<Layer<float16,float16> > layer(new ConvolutionLayer<float16,float16>(layer_param));
+    layer->SetUp(bottom,top);
+    
+    print_shape(top_blob);
+    //EXPECT_EQ(top_blob->num(), 2);
+    //EXPECT_EQ(top_blob->channels(), 4);
+    //EXPECT_EQ(top_blob->height(), 3);
+    //EXPECT_EQ(top_blob->width(), 2);
+    
+    layer->Forward(bottom,top);
+    const float16* top_data = top_blob->cpu_data();
+    for (int i = 0; i < top_blob->count(); ++i)
+        cout << top_data[i] << " ";
+    cout << endl;
 }
