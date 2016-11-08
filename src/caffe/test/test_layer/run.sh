982fb823929d02e39cff0df0cb056f2ec1f72143
Oscar Mañas <oscmansan@gmail.com>
2016-11-08 10:12:45 +0100

    Convolutional layer test working

--- src/caffe/test/test_layer/run.sh
+++ src/caffe/test/test_layer/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PROGRAM=test_layer
 
+make clean
 make $PROGRAM.bin
  
 sudo LD_LIBRARY_PATH=/usr/local/cuda/lib:../../../../.build_release/lib ./$PROGRAM.bin
--- src/caffe/test/test_layer/test_layer.cpp
+++ src/caffe/test/test_layer/test_layer.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
-using namespace std;
-
-#include "gtest/gtest.h"
 #include <assert.h>
+using namespace std;
 
 #include "caffe/blob.hpp"
 #include "caffe/vision_layers.hpp"
@@ -14,76 +12,103 @@ using namespace std;
 #include "caffe/filler.hpp"
 using namespace caffe;
 
-void print_shape(Blob<float16,float16>* blob) {
-    vector<int> shape = blob->shape();
-    cout << "(" << shape[0];
-    for (int i = 1; i < shape.size(); ++i) {
-        cout << "," << shape[i];
+class LayerTest {
+public:
+    LayerTest() {
+        Caffe::set_mode(Caffe::GPU);
+        init();
     }
-    cout << ")" << endl;
-}
 
-void print_blob(Blob<float16,float16>* blob) {
-    cout << "[" << blob->count() << "]" << endl;
-    const float16* data = blob->cpu_data();
-    for (int i = 0; i < blob->count(); ++i)
-        cout << data[i] << " ";
-    cout << endl;
-}
+    void ConvolutionLayerTest() {
+        // Fill bottom blob
+        vector<int> shape {1, 1, 7, 5};
+        bottom_blob->Reshape(shape);
+        init_rand(bottom_blob); print_blob(bottom_blob);
 
-void init_rand(Blob<float16,float16>* blob) {
-    float16* data = blob->mutable_cpu_data();
-    for (int i = 0; i < blob->count(); ++i) {
-        data[i] = Get<float16>(float(rand())/float(RAND_MAX));
+        // Set up layer parameters
+        LayerParameter layer_param;
+        ConvolutionParameter* conv_param = layer_param.mutable_convolution_param();
+        conv_param->add_kernel_size(3);
+        conv_param->add_stride(2);
+        conv_param->set_num_output(4); // number of filters
+
+        // Create layer
+        std::shared_ptr<Layer<float16,float16> > layer(new ConvolutionLayer<float16,float16>(layer_param));
+        layer->SetUp(bottom,top);
+
+        assert(top_blob->num() == 1);
+        assert(top_blob->channels() == 4);
+        assert(top_blob->height() == 3);
+        assert(top_blob->width() == 2);
+
+        // Set up convolution filters
+        Blob<float16,float16>* weights = layer->blobs()[0].get();
+        init_ones(weights);
+        print_blob(weights);
+
+        // Run forward pass
+        layer->Forward(bottom,top);
+        print_blob(top_blob);
     }
-}
 
-void init_ones(Blob<float16,float16>* blob) {
-    float16* data = blob->mutable_cpu_data();
-    for (int i = 0; i < blob->count(); ++i) {
-        data[i] = Get<float16>(1.);
+private:
+    Blob<float16,float16>* bottom_blob;
+    Blob<float16,float16>* top_blob;
+    vector<Blob<float16,float16>*> bottom;
+    vector<Blob<float16,float16>*> top;
+
+    void init() {
+        // Create bottom blob
+        bottom_blob = new Blob<float16,float16>();
+        bottom.push_back(bottom_blob);
+        
+        // Create top blob
+        top_blob = new Blob<float16,float16>();
+        top.push_back(top_blob);
     }
-}
+
+    void print_shape(Blob<float16,float16>* blob) {
+        vector<int> shape = blob->shape();
+        cout << "(" << shape[0];
+        for (int i = 1; i < shape.size(); ++i) {
+            cout << "," << shape[i];
+        }
+        cout << ")" << endl;
+    }
+
+    void print_blob(Blob<float16,float16>* blob) {
+        print_shape(blob);
+        const float16* data = blob->cpu_data();
+        vector<int> shape = blob->shape();
+        for (int i = 0; i < shape[0]*shape[1]; ++i) {
+            for (int j = 0; j < shape[2]; ++j) {
+                for (int k = 0; k < shape[3]; ++k) {
+                    cout << data[i*shape[2]*shape[3]+j*shape[3]+k] << " ";
+                }
+                cout << endl;
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    void init_rand(Blob<float16,float16>* blob) {
+        float16* data = blob->mutable_cpu_data();
+        for (int i = 0; i < blob->count(); ++i) {
+            data[i] = Get<float16>(float(rand())/float(RAND_MAX));
+        }
+    }
+
+    void init_ones(Blob<float16,float16>* blob) {
+        float16* data = blob->mutable_cpu_data();
+        for (int i = 0; i < blob->count(); ++i) {
+            data[i] = Get<float16>(1.);
+        }
+    }
+};
 
 
 int main() {
-    vector<Blob<float16,float16>*> bottom;
-    Blob<float16,float16>* bottom_blob = new Blob<float16,float16>(1, 1, 7, 5);
-    //FillerParameter filler_param;
-    //filler_param.set_value(1.);
-    //GaussianFiller<float16,float16> filler(filler_param);
-    //filler.Fill(bottom_blob); print_blob(bottom_blob);
-    init_rand(bottom_blob); print_blob(bottom_blob);
-    bottom.push_back(bottom_blob);
-
-    vector<Blob<float16,float16>*> top;
-    Blob<float16,float16>* top_blob = new Blob<float16,float16>();
-    top.push_back(top_blob);
-
-    LayerParameter layer_param;
-    ConvolutionParameter* conv_param = layer_param.mutable_convolution_param();
-    conv_param->add_kernel_size(3);
-    conv_param->add_stride(2);
-    conv_param->set_num_output(4); // number of filters
-    conv_param->mutable_weight_filler()->set_type("gaussian"); // type of filters
-
-    shared_ptr<Layer<float16,float16> > layer(new ConvolutionLayer<float16,float16>(layer_param));
-    layer->SetUp(bottom,top);
-
-    Blob<float16,float16>* weights = layer->blobs()[0].get();
-    init_ones(weights); print_blob(weights);
-    
-    print_shape(top_blob);
-    //EXPECT_EQ(top_blob->num(), 2);
-    assert(top_blob->num() == 1);
-    //EXPECT_EQ(top_blob->channels(), 4);
-    assert(top_blob->channels() == 4);
-    //EXPECT_EQ(top_blob->height(), 3);
-    assert(top_blob->height() == 3);
-    //EXPECT_EQ(top_blob->width(), 2);
-    assert(top_blob->width() == 2);
-    
-    layer->Forward(bottom,top);
-
-    print_blob(top_blob);
+    LayerTest test;
+    test.ConvolutionLayerTest();
 }
