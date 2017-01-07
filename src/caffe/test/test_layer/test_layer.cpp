47934a66845a25cdacff9b81aeea4df809114e42
Oscar Mañas <oscmansan@gmail.com>
2017-01-07 20:18:12 +0100

    Updated test_layer

--- src/caffe/test/test_layer/test_layer.cpp
+++ src/caffe/test/test_layer/test_layer.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
+#include <time.h>
 #include <assert.h>
 using namespace std;
 
 #include "caffe/blob.hpp"
 #include "caffe/vision_layers.hpp"
+#include "caffe/common_layers.hpp"
 #include "caffe/proto/caffe.pb.h"
 #include "caffe/util/io.hpp"
 #include "caffe/util/get.hpp"
 #include "caffe/filler.hpp"
 using namespace caffe;
+
+#define Dtype float16
+#define Mtype float16
 
 class LayerTest {
 public:
@@ -21,9 +27,9 @@ public:
 
     void ConvolutionLayerTest() {
         // Fill bottom blob
-        vector<int> shape {1, 1, 7, 5};
-        bottom_blob->Reshape(shape);
-        init_rand(bottom_blob); print_blob(bottom_blob);
+        init_rand(bottom_blob); 
+        cout << "I: " << to_string(bottom_blob->shape()) << endl;
+        clog << to_string(bottom_blob) << endl;
 
         // Set up layer parameters
         LayerParameter layer_param;
@@ -31,84 +37,159 @@ public:
         conv_param->add_kernel_size(3);
         conv_param->add_stride(2);
         conv_param->set_num_output(4); // number of filters
-        conv_param->mutable_weight_filler()->set_type("constant"); // type of filters
-        conv_param->mutable_weight_filler()->set_value(1.0);
+        //conv_param->mutable_weight_filler()->set_type("constant"); // type of filters
+        //conv_param->mutable_weight_filler()->set_value(1.0);
+        //conv_param->mutable_weight_filler()->set_type("gaussian");
 
         // Create layer
-        std::shared_ptr<Layer<float16,float16> > layer(new ConvolutionLayer<float16,float16>(layer_param));
+        std::shared_ptr<Layer<Dtype,Mtype> > layer(new ConvolutionLayer<Dtype,Mtype>(layer_param));
         layer->SetUp(bottom,top);
 
-        assert(top_blob->num() == 1);
+        assert(top_blob->num() == num);
         assert(top_blob->channels() == 4);
-        assert(top_blob->height() == 3);
+        assert(top_blob->height() == 1);
         assert(top_blob->width() == 2);
 
-        Blob<float16,float16>* weights = layer->blobs()[0].get();
-        print_blob(weights);
+        Blob<Dtype,Mtype>* weights = layer->blobs()[0].get();
+        init_rand(weights);
+        cout << "W: " << to_string(weights->shape()) << endl;
+        clog << to_string(weights) << endl;
 
         // Run forward pass
+        timespec start,end,elapsed;
+        clock_gettime(CLOCK_REALTIME,&start);
         layer->Forward(bottom,top);
-        print_blob(top_blob);
+        clock_gettime(CLOCK_REALTIME,&end);
+        cout << "O: " << to_string(top_blob->shape()) << endl;
+        clog << to_string(top_blob) << endl;
+        elapsed = diff(start,end);
+        cout<<"time: "<<elapsed.tv_sec*1000000000+elapsed.tv_nsec<<endl;
+    }
+
+    void InnerProductLayerTest() {
+        // Fill bottom blob
+        init_rand(bottom_blob);
+        cout << "I: " << to_string(bottom_blob->shape()) << endl; 
+        clog << to_string(bottom_blob) << endl;
+
+        // Set up layer parameters
+        LayerParameter layer_param;
+        InnerProductParameter* inner_product_param = layer_param.mutable_inner_product_param();
+        inner_product_param->set_num_output(10);
+        //inner_product_param->mutable_weight_filler()->set_type("constant");
+        //inner_product_param->mutable_weight_filler()->set_value(1.0);
+        //inner_product_param->mutable_weight_filler()->set_type("uniform");
+
+        // Create layer
+        std::shared_ptr<Layer<Dtype,Mtype> > layer(new InnerProductLayer<Dtype,Mtype>(layer_param));
+        layer->SetUp(bottom,top);
+
+        assert(top_blob->num() == num);
+        assert(top_blob->channels() == 10);
+        assert(top_blob->height() == 1);
+        assert(top_blob->width() == 1);
+
+        Blob<Dtype,Mtype>* weights = layer->blobs()[0].get();
+        init_rand(weights);
+        cout << "W: " << to_string(weights->shape()) << endl;
+        clog << to_string(weights) << endl;
+
+        // Run forward pass
+        timespec start,end,elapsed;
+        clock_gettime(CLOCK_REALTIME,&start);
+        layer->Forward(bottom,top);
+        clock_gettime(CLOCK_REALTIME,&end);
+        cout << "O: " << to_string(top_blob->shape()) << endl;
+        clog << to_string(top_blob) << endl;
+        elapsed = diff(start,end);
+        cout<<"time: "<<elapsed.tv_sec*1000000000+elapsed.tv_nsec<<endl;
     }
 
 private:
-    Blob<float16,float16>* bottom_blob;
-    Blob<float16,float16>* top_blob;
-    vector<Blob<float16,float16>*> bottom;
-    vector<Blob<float16,float16>*> top;
+    int num = 2;
+    int channels = 3;
+    int height = 4;
+    int width = 5;
+
+    Blob<Dtype,Mtype>* bottom_blob;
+    Blob<Dtype,Mtype>* top_blob;
+    vector<Blob<Dtype,Mtype>*> bottom;
+    vector<Blob<Dtype,Mtype>*> top;
+
+    ofstream ofs;
 
     void init() {
         // Create bottom blob
-        bottom_blob = new Blob<float16,float16>();
+        bottom_blob = new Blob<Dtype,Mtype>();
         bottom.push_back(bottom_blob);
+
+        // Reshape bottom blob
+        vector<int> shape {num, channels, height, width};
+        bottom_blob->Reshape(shape);
         
         // Create top blob
-        top_blob = new Blob<float16,float16>();
+        top_blob = new Blob<Dtype,Mtype>();
         top.push_back(top_blob);
+
+        ofs.open("test_layer.log");
+        clog.rdbuf(ofs.rdbuf());
     }
 
-    void print_shape(Blob<float16,float16>* blob) {
-        vector<int> shape = blob->shape();
-        cout << "(" << shape[0];
+    string to_string(vector<int> shape) {
+        string s = "";
+        s += "(" + std::to_string(shape[0]);
         for (int i = 1; i < shape.size(); ++i) {
-            cout << "," << shape[i];
+            s += "," + std::to_string(shape[i]);
         }
-        cout << ")" << endl;
+        s += ")";
+        return s;
     }
 
-    void print_blob(Blob<float16,float16>* blob) {
-        print_shape(blob);
-        const float16* data = blob->cpu_data();
-        vector<int> shape = blob->shape();
-        for (int i = 0; i < shape[0]*shape[1]; ++i) {
-            for (int j = 0; j < shape[2]; ++j) {
-                for (int k = 0; k < shape[3]; ++k) {
-                    cout << data[i*shape[2]*shape[3]+j*shape[3]+k] << " ";
-                }
-                cout << endl;
-            }
-            cout << endl;
+    string to_string(Blob<Dtype,Mtype>* blob) {
+        const Dtype* data = blob->cpu_data();
+        int n = blob->num();
+        int c = blob->channels();
+        int h = blob->height();
+        int w = blob->width();
+        string s = "";
+        for (int i = 0; i < n*c*h*w; ++i) {
+            s += std::to_string(data[i]) + " ";
         }
-        cout << endl;
+        return s;
     }
 
-    void init_rand(Blob<float16,float16>* blob) {
-        float16* data = blob->mutable_cpu_data();
+    void init_rand(Blob<Dtype,Mtype>* blob) {
+        Dtype* data = blob->mutable_cpu_data();
         for (int i = 0; i < blob->count(); ++i) {
             data[i] = Get<float16>(float(rand())/float(RAND_MAX));
         }
     }
 
-    void init_ones(Blob<float16,float16>* blob) {
-        float16* data = blob->mutable_cpu_data();
+    void init_ones(Blob<Dtype,Mtype>* blob) {
+        Dtype* data = blob->mutable_cpu_data();
+        srand(1234);
         for (int i = 0; i < blob->count(); ++i) {
             data[i] = Get<float16>(1.);
         }
+    }
+
+    timespec diff(timespec start, timespec end) {
+        timespec temp;
+        if ((end.tv_nsec-start.tv_nsec)<0) {
+            temp.tv_sec = end.tv_sec-start.tv_sec-1;
+            temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+        } 
+        else {
+            temp.tv_sec = end.tv_sec-start.tv_sec;
+            temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+       }
+       return temp;
     }
 };
 
 
 int main() {
     LayerTest test;
-    test.ConvolutionLayerTest();
+    //test.ConvolutionLayerTest();
+    test.InnerProductLayerTest();
 }
